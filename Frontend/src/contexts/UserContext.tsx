90905a68ccd0371033de48b86f8b51f219ba8152
Anderson <andersonpaulojr1@gmail.com>
2025-05-30 09:44:43 -0300

    refactor: remove comments

--- Frontend/src/contexts/UserContext.tsx
+++ Frontend/src/contexts/UserContext.tsx
@@ -2,11 +2,6 @@ import { createContext } from "react";
 
 import type { UserContextType } from "../types/types";
 
-// Define the shape of the user data
-
-// 1. Create the Context
-// We can provide a default value, which is useful for testing or when a
-// component tries to consume the context without a Provider higher up in the tree.
 const defaultUserState: UserContextType = {
   id: null,
   name: "",
--- Frontend/src/types/types.ts
+++ Frontend/src/types/types.ts
@@ -1,4 +1,3 @@
-// src/types.ts
 export interface Asset {
   id: string;
   user_id: string;
